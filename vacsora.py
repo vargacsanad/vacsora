92d280560c6695c5e1a3bbd945e10cfac91a8a2c
vargacsanad <93704071+vargacsanad@users.noreply.github.com>
2022-12-06 12:17:10 +0100

    Add files via upload

---
+++ vacsora.py
@@ -0,0 +1,23 @@
+# reggeli = {'Víz','tea','vaj','pirítós'}
+# ebed = { 'víz','halászlé','kenyér'}
+
+# print (reggeli & ebed)
+# print(reggeli | ebed)
+# print(reggeli - ebed)
+# print(reggeli ^ ebed)
+
+# gyumolcskosar = ['eper','alma','szilva','barack','körte']
+# fajta=set()
+# for gyumolcs in gyumolcskosar:
+#     fajta.add(gyumolcs)
+# print(fajta)
+
+
+
+vacsora = {'krumplistarhonya','husleves','paradicsomszosz','aranygaluska'}
+print(vacsora)
+vacsora.pop()
+vacsora.remove('husleves')
+print(vacsora)
+vacsora.clear
+print(vacsora)
